27c5dc8181e8c9dceb05fec05f4c324371aa69f7
dannych <dnny.ch@gmail.com>
2016-10-29 20:27:32 +0700

    add course #8

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      red   : 128,
+      green : 128,
+      blue  : 128,
+    };
+    this.update = this.update.bind(this);
+
+  }
+  render() {
+    return (
+      <div>
+        <Slider ref="red" val={this.state.red} update={this.update} />
+        <Slider ref="green" val={this.state.green} update={this.update} />
+        <Slider ref="blue" val={this.state.blue} update={this.update} />      
+      </div>
+    );
+  }
+  update(e) {
+    this.setState({ 
+      red: ReactDOM.findDOMNode(this.refs.red.refs.slider).value,  
+      green: ReactDOM.findDOMNode(this.refs.green.refs.slider).value,  
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.slider).value,  
+    });
+  }
+}
+
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="slider" type="range" min="0" max="255" onChange={this.props.update} value={this.props.val} />
+        <span>{this.props.val}</span>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/react-autosuggest/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
